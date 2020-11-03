4667e900353f9c425ebb675f86ce54f7f97c6599
dlscoccia <2054@holbertonschool.com>
2020-11-03 20:35:59 +0000

    Updating test_console.py adding for loops; fixing shebang review.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,12 @@ from io import StringIO
 from unittest.mock import patch
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from console import HBNBCommand
 from console import classes
 
@@ -12,8 +18,8 @@ from console import classes
 class ConsoleTest(unittest.TestCase):
     '''Class to test the Console'''
 
-    str_id = "None"
-    my_class = 'BaseModel'  # list(classes.keys())[0]
+    str_id = []
+    my_classes = list(classes.keys())
 
     def test_00_help(self):
         '''Checks the help command'''
@@ -44,22 +50,23 @@ class ConsoleTest(unittest.TestCase):
         self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create " + self.my_class)
-            str_test = f.getvalue().rstrip()
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("create " + test_class)
+                str_test = f.getvalue().rstrip()
 
-        exp_len = 36  # the total len of the id
-        exp_sublen = 4  # len of time_mid, time_hi_version, etc fields of id
-        str_test_len = len(str_test)
-        str_test_split = str_test.split('-')
-        if str_test_len == exp_len:
-            ConsoleTest.str_id = str_test  # id going to be used in other tests
+            exp_len = 36  # the total len of the id
+            exp_sublen = 4  # len of time_mid, time_hi_version fields of id
+            str_test_len = len(str_test)
+            str_test_split = str_test.split('-')
+            if str_test_len == exp_len:
+                ConsoleTest.str_id.append(str_test)  # going to be used later
 
-        # -- to validate the correct structure of the id
-        self.assertEqual(str_test_len, exp_len)
-        self.assertEqual(len(str_test_split[1]), exp_sublen)
-        self.assertEqual(len(str_test_split[2]), exp_sublen)
-        self.assertEqual(len(str_test_split[3]), exp_sublen)
+            # -- to validate the correct structure of the id
+            self.assertEqual(str_test_len, exp_len)
+            self.assertEqual(len(str_test_split[1]), exp_sublen)
+            self.assertEqual(len(str_test_split[2]), exp_sublen)
+            self.assertEqual(len(str_test_split[3]), exp_sublen)
 
     def test_02_show(self):
         '''Checks the show command'''
@@ -81,32 +88,38 @@ class ConsoleTest(unittest.TestCase):
         self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname and no id
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("show " + self.my_class)
-            str_test = f.getvalue().rstrip()
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("show " + test_class)
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** instance id missing **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** instance id missing **"
+            self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname with an id
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("show " + self.my_class + " " + self.str_id)
-            str_test = f.getvalue().rstrip()
+        j = 0
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("show " + test_class + " " +
+                                     self.str_id[j])
+                str_test = f.getvalue().rstrip()
 
-        split = str_test.split()
-        str_test_class = split[0].lstrip("[").rstrip("]")
-        str_test_id = split[1].lstrip("(").rstrip(")")
+            split = str_test.split()
+            str_test_class = split[0].lstrip("[").rstrip("]")
+            str_test_id = split[1].lstrip("(").rstrip(")")
 
-        self.assertEqual(str_test_class, self.my_class)
-        self.assertEqual(str_test_id, self.str_id)
+            self.assertEqual(str_test_class, test_class)
+            self.assertEqual(str_test_id, self.str_id[j])
+            j += 1
 
         # checks the command with an existing classname with a non existing id
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("show " + self.my_class + " " + "1234")
-            str_test = f.getvalue().rstrip()
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("show " + test_class + " " + "1234")
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** no instance found **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** no instance found **"
+            self.assertEqual(str_test, str_exp)
 
     def test_03_destroy(self):
         '''Checks the destroy command'''
@@ -128,29 +141,36 @@ class ConsoleTest(unittest.TestCase):
         self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname and no id
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("destroy " + self.my_class)
-            str_test = f.getvalue().rstrip()
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("destroy " + test_class)
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** instance id missing **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** instance id missing **"
+            self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname with a non existing id
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("destroy " + self.my_class + " " + "1234")
-            str_test = f.getvalue().rstrip()
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("destroy " + test_class + " " + "1234")
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** no instance found **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** no instance found **"
+            self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname with an id
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("destroy " + self.my_class + " "+self.str_id)
-            HBNBCommand().onecmd("show " + self.my_class + " " + self.str_id)
-            str_test = f.getvalue().rstrip()
+        j = 0
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("destroy " + test_class + " " +
+                                     self.str_id[j])
+                HBNBCommand().onecmd("show " + test_class + " " +
+                                     self.str_id[j])
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** no instance found **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** no instance found **"
+            self.assertEqual(str_test, str_exp)
+            j += 1
 
     def test_04_all(self):
         '''Checks the all command'''
@@ -180,22 +200,23 @@ class ConsoleTest(unittest.TestCase):
         self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("all " + self.my_class)
-            str_test = f.getvalue().rstrip()
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("all " + test_class)
+                str_test = f.getvalue().rstrip()
 
-        my_dict = storage.all()
-        exp_len = 0
-        for key, value in my_dict.items():
-            key = key.split('.')[0]
-            if key == self.my_class:
-                exp_len += len(str(value)) + 1
-        if exp_len > 0:
-            exp_len -= 1
-        len_str_test = len(str_test)
+            my_dict = storage.all()
+            exp_len = 0
+            for key, value in my_dict.items():
+                key = key.split('.')[0]
+                if key == test_class:
+                    exp_len += len(str(value)) + 1
+            if exp_len > 0:
+                exp_len -= 1
+            len_str_test = len(str_test)
 
-        # the output of all is validated through the len of str
-        self.assertEqual(len_str_test, exp_len)
+            # the output of all is validated through the len of str
+            self.assertEqual(len_str_test, exp_len)
 
     def test_05_update(self):
         '''Checks the update command'''
@@ -217,83 +238,101 @@ class ConsoleTest(unittest.TestCase):
         self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname and no id
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("update " + self.my_class)
-            str_test = f.getvalue().rstrip()
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update " + test_class)
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** instance id missing **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** instance id missing **"
+            self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname with a non existing id
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("update " + self.my_class + " " + "1234")
-            str_test = f.getvalue().rstrip()
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update " + test_class + " " + "1234")
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** no instance found **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** no instance found **"
+            self.assertEqual(str_test, str_exp)
 
         # checks the command with an existing classname
         #     with a valid id and no argument
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create " + self.my_class)
-            str_test_id = f.getvalue().rstrip()
-            ConsoleTest.str_id = str_test_id
+        j = 0
+        ConsoleTest.str_id = []
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("create " + test_class)
+                str_test_id = f.getvalue().rstrip()
+                ConsoleTest.str_id.append(str_test_id)
 
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("update " + self.my_class + " " + self.str_id)
-            str_test = f.getvalue().rstrip()
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update " + test_class + " " +
+                                     self.str_id[j])
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** attribute name missing **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** attribute name missing **"
+            self.assertEqual(str_test, str_exp)
+            j += 1
 
         # checks the command with an existing classname
         #     with a valid id, valid argument and no value
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("update " + self.my_class + " " +
-                                 self.str_id + " name")
-            str_test = f.getvalue().rstrip()
+        j = 0
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update " + test_class + " " +
+                                     self.str_id[j] + " name")
+                str_test = f.getvalue().rstrip()
 
-        str_exp = "** value missing **"
-        self.assertEqual(str_test, str_exp)
+            str_exp = "** value missing **"
+            self.assertEqual(str_test, str_exp)
+            j += 1
 
         # checks the command with an existing classname
         #     with a valid id, valid argument and a valid value
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("update " + self.my_class + " "+self.str_id +
-                                 " Fullname " + '"Betty Holberton"')
-            str_test = f.getvalue().rstrip()
+        j = 0
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update " + test_class + " " +
+                                     self.str_id[j] + " Fullname " +
+                                     '"Betty Holberton"')
+                str_test = f.getvalue().rstrip()
 
-        key = self.my_class + "." + self.str_id
-        test_dict = storage.all()
-        obj = test_dict[key]
-        obj_dict = obj.to_dict()
+            key = test_class + "." + self.str_id[j]
+            test_dict = storage.all()
+            obj = test_dict[key]
+            obj_dict = obj.to_dict()
 
-        exp_date = obj.created_at
-        test_date = obj.updated_at
+            exp_date = obj.created_at
+            test_date = obj.updated_at
 
-        self.assertNotEqual(test_date, exp_date)
-        self.assertTrue('Fullname' in obj_dict)
-        self.assertTrue("Betty Holberton" in obj_dict.values())
+            self.assertNotEqual(test_date, exp_date)
+            self.assertTrue('Fullname' in obj_dict)
+            self.assertTrue("Betty Holberton" in obj_dict.values())
+            j += 1
 
         # checks the command with an existing classname
         #     with a valid id, valid argument, a valid value and only
         #     updates one atribute
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("update " + self.my_class + " "+self.str_id +
-                                 " Fullname " + '"Betty Holberton"' + " age " +
-                                 "52")
-            str_test = f.getvalue().rstrip()
+        j = 0
+        for test_class in self.my_classes:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("update " + test_class + " " +
+                                     self.str_id[j] + " Fullname " +
+                                     '"Betty Holberton"' + " age " +
+                                     "52")
+                str_test = f.getvalue().rstrip()
 
-        key = self.my_class + "." + self.str_id
-        test_dict = storage.all()
-        obj = test_dict[key]
-        obj_dict = obj.to_dict()
+            key = test_class + "." + self.str_id[j]
+            test_dict = storage.all()
+            obj = test_dict[key]
+            obj_dict = obj.to_dict()
 
-        exp_date = obj.created_at
-        test_date = obj.updated_at
+            exp_date = obj.created_at
+            test_date = obj.updated_at
 
-        self.assertNotEqual(test_date, exp_date)
-        self.assertTrue('Fullname' in obj_dict)
-        self.assertTrue("Betty Holberton" in obj_dict.values())
-        self.assertFalse('age' in obj_dict)
-        self.assertFalse("52" in obj_dict.values())
+            self.assertNotEqual(test_date, exp_date)
+            self.assertTrue('Fullname' in obj_dict)
+            self.assertTrue("Betty Holberton" in obj_dict.values())
+            self.assertFalse('age' in obj_dict)
+            self.assertFalse("52" in obj_dict.values())
+            j += 1
