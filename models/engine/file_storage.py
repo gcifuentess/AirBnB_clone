76c093e8ed2a239bedd47eb064d8d4bc56d89b7d
Gabriel Cifuentes <1795@holbertonschool.com>
2020-11-04 04:22:06 +0000

    adding models/engine/file_storage.py v1.2; class_attributes method added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''File Storage module '''
 import json
+import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
@@ -51,3 +52,28 @@ class FileStorage:
         if key in self.__objects:
             del self.__objects[key]
         self.save()
+
+    def class_attributes(self):
+        '''returns a dict with all models attibutes'''
+
+        attrbs = {'BaseModel': {'id': str, 'created_at': datetime.datetime,
+                                'updated_at': datetime.datetime},
+
+                  'User': {'email': str, 'password': str, 'first_name': str,
+                           'last_name': str},
+
+                  'State': {'name': str},
+
+                  'City': {'state_id': str, 'name': str},
+
+                  'Amenity': {'name': str},
+
+                  'Place': {'city_id': str, 'user_id': str, 'name': str,
+                            'description': str, 'number_rooms': int,
+                            'number_bathrooms': int, 'max_guest': int,
+                            'price_by_night': int, 'latitude': float,
+                            'longitude': float, 'amenity_ids': list},
+
+                  'Review': {'place_id': str, 'user_id': str, 'text': str}}
+
+        return attrbs
